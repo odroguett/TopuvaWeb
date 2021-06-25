a3348c239f35a03a7c87252f3277ad4c7f42a04c
osvaldo droguett <odroguett@gmail.com>
2021-06-24 23:50:33 -0400

    Creación log aplicación.

--- Negocio/despacho.php
+++ Negocio/despacho.php
@@ -1,54 +1,68 @@
 <?php 
-
 require_once("../BD/catalogoBD.php");
+require_once("../log/log.php");
+try
 
-$oCatalogo= new catalogoBD();
-$oRespuesta = new RespuestaOtd();
-$sModificar= filter_var($_POST['modificar'], FILTER_SANITIZE_STRING); 
-$idDespacho= filter_var($_POST['idDespacho'],FILTER_SANITIZE_NUMBER_INT); 
-$sNombre = filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);  
-$sApellidos = filter_var($_POST['apellido'],FILTER_SANITIZE_STRING); 
-$sDireccion = filter_var($_POST['direccion'],FILTER_SANITIZE_STRING); 
-$sDepartamento = filter_var($_POST['departamento'],FILTER_SANITIZE_STRING); 
-$sCiudad = filter_var($_POST['ciudad'],FILTER_SANITIZE_STRING); 
-$sComuna = filter_var($_POST['comuna'],FILTER_SANITIZE_STRING); 
-$sRegion = filter_var($_POST['region'],FILTER_SANITIZE_STRING);
-$sTelefono =  filter_var($_POST['telefono'],FILTER_SANITIZE_STRING);
-$sEmail = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
-
-// Acaba vamos a realizar control de limpieza de imput.
-if($sModificar=="C")
 {
-
-    $oRespuesta =$oCatalogo->InsertaDespacho($sNombre,$sApellidos,$sDireccion,$sDepartamento,$sCiudad,$sComuna,$sRegion,$sTelefono,$sEmail);
-}
-else
-{
-    $oRespuesta =$oCatalogo->ActualizaDespacho($sNombre,$sApellidos,$sDireccion,$sDepartamento,$sCiudad,$sComuna,$sRegion,$sTelefono,$sEmail,$idDespacho);
-
-}
-
-
-if($oRespuesta->bEsValido)
-{
-
+    $oLog = new Log();
+    $oCatalogo= new catalogoBD();
+    $oRespuesta = new RespuestaOtd();
+    $sModificar= filter_var($_POST['modificar'], FILTER_SANITIZE_STRING); 
+    $idDespacho= filter_var($_POST['idDespacho'],FILTER_SANITIZE_NUMBER_INT); 
+    $sNombre = filter_var($_POST['nombre'],FILTER_SANITIZE_STRING);  
+    $sApellidos = filter_var($_POST['apellido'],FILTER_SANITIZE_STRING); 
+    $sDireccion = filter_var($_POST['direccion'],FILTER_SANITIZE_STRING); 
+    $sDepartamento = filter_var($_POST['departamento'],FILTER_SANITIZE_STRING); 
+    $sCiudad = filter_var($_POST['ciudad'],FILTER_SANITIZE_STRING); 
+    $sComuna = filter_var($_POST['comuna'],FILTER_SANITIZE_STRING); 
+    $sRegion = filter_var($_POST['region'],FILTER_SANITIZE_STRING);
+    $sTelefono =  filter_var($_POST['telefono'],FILTER_SANITIZE_STRING);
+    $sEmail = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
     
-    $mensaje= array('bEsValido' =>$oRespuesta->bEsValido, 'idcliente' => 0, 'respuesta' =>  $oRespuesta->sMensaje,'direccion' => $oRespuesta->sDireccion,'comuna'=>$oRespuesta->sComuna,
-                    'ciudad'=>$oRespuesta->sCiudad,'region'=>$oRespuesta->sRegion,'telefono'=>$oRespuesta->sTelefono, 
-                    'idDespacho'=>$oRespuesta->idDespacho,'departamento' => $oRespuesta->sDepartamento,'sEmail' => $oRespuesta->sEmail);
-    echo json_encode($mensaje,JSON_FORCE_OBJECT);
+    $oLog->EscribeLog("ERROR","PRUEBA","");
+    // Acaba vamos a realizar control de limpieza de imput.
+    if($sModificar=="C")
+    {
     
-    exit();
+        $oRespuesta =$oCatalogo->InsertaDespacho($sNombre,$sApellidos,$sDireccion,$sDepartamento,$sCiudad,$sComuna,$sRegion,$sTelefono,$sEmail);
+    }
+    else
+    {
+        $oRespuesta =$oCatalogo->ActualizaDespacho($sNombre,$sApellidos,$sDireccion,$sDepartamento,$sCiudad,$sComuna,$sRegion,$sTelefono,$sEmail,$idDespacho);
+    
+    }
+    
+    
+    if($oRespuesta->bEsValido)
+    {
+    
+        
+        $mensaje= array('bEsValido' =>$oRespuesta->bEsValido, 'idcliente' => 0, 'respuesta' =>  $oRespuesta->sMensaje,'direccion' => $oRespuesta->sDireccion,'comuna'=>$oRespuesta->sComuna,
+                        'ciudad'=>$oRespuesta->sCiudad,'region'=>$oRespuesta->sRegion,'telefono'=>$oRespuesta->sTelefono, 
+                        'idDespacho'=>$oRespuesta->idDespacho,'departamento' => $oRespuesta->sDepartamento,'sEmail' => $oRespuesta->sEmail);
+        echo json_encode($mensaje,JSON_FORCE_OBJECT);
+        
+        exit();
+    
+    }
+    else
+    {
+        $oRespuesta->$sMensaje =" Datos del despacho con errores, contacte a soprte";
+        $mensaje= array('idCliente' => $oRespuesta->idCliente, 'respuesta' => $oRespuesta->sMensaje);
+        echo json_encode($mensaje,JSON_FORCE_OBJECT);
+        exit();
+    
+    
+    }
+    
 
 }
-else
+catch(Exception $e)
 {
-    $oRespuesta->$sMensaje =" Datos del despacho con errores, contacte a soprte";
-    $mensaje= array('idCliente' => $oRespuesta->idCliente, 'respuesta' => $oRespuesta->sMensaje);
-    echo json_encode($mensaje,JSON_FORCE_OBJECT);
-    exit();
 
 
 }
+
+
 
 ?>
---
+++ log/log.php
@@ -0,0 +1,87 @@
+<?php 
+
+class Log
+{
+const ERROR = "ERROR";
+const ADVERTENCIA="ADVERTENCIA";
+const INFORMACION ="INFORMACION";
+public $archivo="";
+
+public function crearArchivo($nombreArchivo)
+{
+    try
+    {
+        $this->archivo = fopen($nombreArchivo, "a");
+        if( $this->archivo == false ) {
+          return false;
+        }
+        else
+        {
+         return true;
+
+        }
+
+    }
+    catch(Exception $e)
+    {
+
+    }
+
+    
+
+}
+
+
+public function cerrarArchivo()
+{
+
+    fclose($this->archivo);
+
+}
+
+public function EscribeLog($tipoInformacion,$error,$traza)
+{
+
+    try
+    {
+       if( $this->crearArchivo("../log/LogConsultas.txt"))
+       {
+        $dt = new DateTime('today');
+        $date = $dt->format('m/d/Y');
+        fwrite($this->archivo,  $date .  "\r\n" . PHP_EOL);
+        fwrite($this->archivo, $tipoInformacion . ' ' . $error . ' ' .  PHP_EOL);
+        fflush($this->archivo);
+        $this->cerrarArchivo();
+
+       }
+       else
+       {
+        $this->cerrarArchivo();
+
+       }
+
+        
+
+    }
+
+    catch(Exception $e)
+    {
+
+        $this->cerrarArchivo();
+    }
+
+
+    
+}
+
+public function EscribeLogBaseDatos()
+{
+
+
+}
+
+
+
+}
+
+?>
